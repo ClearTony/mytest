4bdeb8d9f6d1ddcb4c23490253b19f07e5a3d367
cjt <903984996@qq.com>
2018-12-24 20:45:21 +0800

    第一次提交

---
+++ text/src/main/java/com/hupu/test.java
@@ -0,0 +1,14 @@
+/*
+  Created by IntelliJ IDEA.
+  User: Tony
+  Date: 2018/12/24
+  Time: 20:38
+  To change this template use File | Settings | File Templates.
+*/
+package com.hupu;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World！");
+    }
+}
